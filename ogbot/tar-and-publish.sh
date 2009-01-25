f34b056cc5aaeb0333ae1d8c70588a531f792d1e
King.Vash <King.Vash@ea4fc016-181b-0410-ab31-775ce10b74e9>
2009-01-25 03:01:50 +0000

    Released as v3.1a

---
+++ ogbot/tar-and-publish.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERSION=$(cat src/version.txt)
+FILENAME=kovans-ogbot-source_$VERSION.tar.gz
+
+echo "Filename will be: $FILENAME"
+
+
+tar czvf $FILENAME --exclude='*.svn' --exclude='*.pyc' --exclude='*~' --exclude='*.pyo' lib src languages runbot.* *.txt
+svn commit --message "Released as v$VERSION"
+svn copy https://kovans-ogbot.googlecode.com/svn/trunk/ogbot https://kovans-ogbot.googlecode.com/svn/tags/$VERSION -m "Released as v$VERSION"
+./googlecode-upload.py -s "Kovan's OGBot sources $VERSION" -p kovans-ogbot -u kovansogbot -l "Type-Source, OpSys-All" $FILENAME
